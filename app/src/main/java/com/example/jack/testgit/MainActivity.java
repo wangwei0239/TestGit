b5fe31e723afc39c9db2b2ab16d4cb7683a49087
weiwang <weiwang@emotibot.com>
2017-05-18 18:49:42 +0800

    commit after stash

--- app/src/main/java/com/example/jack/testgit/MainActivity.java
+++ app/src/main/java/com/example/jack/testgit/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hahaha");
+        System.out.println("master branch stash 2");
     }
 }
